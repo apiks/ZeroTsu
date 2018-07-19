1d5c9b8d930f349cda8f64373c571e491b2c7eed
apiks <apiks@users.noreply.github.com>
2018-07-19 15:52:31 +0300

    Say command tweak

--- commands/Say.go
+++ commands/Say.go
@@ -49,11 +49,14 @@ import (
 //Sends a message from the bot to the channel
 func sayCommand(s *discordgo.Session, m *discordgo.Message) {
 
-	//Pulls the sentence from strings after "say "
-	sentence := strings.Replace(m.Content, config.BotPrefix+"say ", "", -1)
+	if m.Content != config.BotPrefix + "say" {
 
-	//Sends the sentence to the channel the original message was in.
-	s.ChannelMessageSend(m.ChannelID, sentence)
+		//Pulls the sentence from strings after "say "
+		sentence := strings.Replace(m.Content, config.BotPrefix+"say ", "", -1)
+
+		//Sends the sentence to the channel the original message was in.
+		s.ChannelMessageSend(m.ChannelID, sentence)
+	}
 }
 
 func init() {
