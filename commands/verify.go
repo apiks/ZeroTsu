1af4fd870e0456c881b84404fa41388f9703db6c
apiks <apiks@users.noreply.github.com>
2018-07-25 17:08:44 +0300

    Verification command made functional

--- commands/verify.go
+++ commands/verify.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 
@@ -62,35 +63,93 @@ func verifyCommand(s *discordgo.Session, m *discordgo.Message) {
 		redditUsername = strings.TrimPrefix(redditUsername, "u/")
 	}
 
-	// Add reddit username in map
-	misc.MapMutex.Lock()
-	misc.MemberInfoMap[userID].RedditUsername = redditUsername
-	misc.MapMutex.Unlock()
+	//Pulls info on user
+	userMem, err := s.State.Member(config.ServerID, userID)
+	if err != nil {
+		userMem, err = s.GuildMember(config.ServerID, userID)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
 
-	// Writes modified memberInfo map to storage
-	misc.MemberInfoWrite(misc.MemberInfoMap)
+	// Add reddit username in map
+	if misc.MemberInfoMap[userID] != nil {
+
+		//Stores time of verification
+		t := time.Now()
+		z, _ := t.Zone()
+		ver := t.Format("2006-01-02 15:04:05") + " " + z
+
+		// Sets verification variables
+		misc.MapMutex.Lock()
+		misc.MemberInfoMap[userID].RedditUsername = redditUsername
+		misc.MemberInfoMap[userID].VerifiedDate = ver
+		misc.MapMutex.Unlock()
+
+		// Writes modified memberInfo map to storage
+		misc.MemberInfoWrite(misc.MemberInfoMap)
+
+		// Prints success
+		_, err := s.ChannelMessageSend(m.ChannelID, "Success. Verified "+userMem.User.Mention()+" with "+redditUsername)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+	} else {
+
+		// Prints error
+		_, err := s.ChannelMessageSend(m.ChannelID, "Error: User not in memberInfo. Initializing user and verifying.")
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+
+		// Initializes user in memberInfo.json
+		misc.InitializeUser(userMem)
+
+		//Stores time of verification
+		t := time.Now()
+		z, _ := t.Zone()
+		ver := t.Format("2006-01-02 15:04:05") + " " + z
+
+		// Sets verification variables
+		misc.MapMutex.Lock()
+		misc.MemberInfoMap[userID].RedditUsername = redditUsername
+		misc.MemberInfoMap[userID].VerifiedDate = ver
+		misc.MapMutex.Unlock()
+
+		// Writes modified memberInfo map to storage
+		misc.MemberInfoWrite(misc.MemberInfoMap)
+
+		// Prints success
+		_, err = s.ChannelMessageSend(m.ChannelID, "Success. Verified "+ userMem.User.Mention() + " with " + redditUsername)
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+	}
 
 	// Initializes var roleID which will keep the Verified role ID
 	var roleID string
 
-	// Puts all server roles in roles variable
+	// Puts all server roles in roles
 	roles, err := s.GuildRoles(config.ServerID)
 	if err != nil {
 
 		fmt.Println("Error:", err)
+	}
 
-		// Fetches ID of Verified role and finds the correct one
-		for i := 0; i < len(roles); i++ {
-			if roles[i].Name == "Verified" {
+	// Fetches ID of Verified role and finds the correct one
+	for i := 0; i < len(roles); i++ {
+		if roles[i].Name == "Verified" {
 
-				roleID = roles[i].ID
-			}
+			roleID = roles[i].ID
 		}
+	}
 
-		// Assigns verified role to user
-		err = s.GuildMemberRoleAdd(config.ServerID, userID, roleID)
+	// Assigns verified role to user
+	err = s.GuildMemberRoleAdd(config.ServerID, userID, roleID)
+	if err != nil {
+
+		_, err := s.ChannelMessageSend(m.ChannelID, "Error: Verified role not found.")
 		if err != nil {
-
 			fmt.Println("Error:", err)
 		}
 	}
