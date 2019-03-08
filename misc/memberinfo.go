a44cbbda95bd5a367de8c2b79eebc13316fe3937
apiks <apiks@users.noreply.github.com>
2019-03-08 18:34:30 +0200

    Fixed build job

--- misc/memberinfo.go
+++ misc/memberinfo.go
@@ -41,7 +41,6 @@ type UserInfo struct {
 	VerifiedDate   string   			`json:"verifiedDate,omitempty"`
 	UnbanDate      string   			`json:"unbanDate,omitempty"`
 	Timestamps 	   []Punishment			`json:"timestamps,omitempty"`
-	Waifu		   Waifu				`json:"waifu,omitempty"`
 }
 
 // Creates a struct type in which we'll hold every banned user
