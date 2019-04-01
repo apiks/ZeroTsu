8a12b55d3c2673c55b2e0dba529ed280b2c6ee5f
apiks <apiks@users.noreply.github.com>
2019-04-01 13:12:22 +0300

    help waifus test case

--- commands/help.go
+++ commands/help.go
@@ -843,6 +843,7 @@ func helpWaifuEmbed(s *discordgo.Session, m *discordgo.Message) error {
 
 	// Adds everything together
 	embedMess.Fields = embed
+	fmt.Println(&commandsField)
 
 	// Sends embed in channel
 	_, err := s.ChannelMessageSendEmbed(m.ChannelID, &embedMess)
