11cbe3da10d8d76ec17e440723d9e793fe53e6c0
apiks <apiks@users.noreply.github.com>
2019-04-07 20:59:48 +0300

    Tweaked about command to display current state user username instead of hardcoded name

--- commands/about.go
+++ commands/about.go
@@ -9,8 +9,8 @@ import (
 
 // Returns a message for bot information
 func aboutCommand(s *discordgo.Session, m *discordgo.Message) {
-	_, err := s.ChannelMessageSend(m.ChannelID, "Hello, darling. I'm ZeroTsu and was made by Professor Apiks for /r/anime. I'm written in Go. "+
-		"He says I'm from Darling in the Franxx but that's just a bunch of nonsense to me. Use `" + config.BotPrefix +
+	_, err := s.ChannelMessageSend(m.ChannelID, "Hello, darling. I'm " + s.State.User.Username + " and was made by Professor Apiks for /r/anime. I'm written in Go. "+
+		"He says the original me is from Darling in the Franxx but that's just a bunch of nonsense to me. Use `" + config.BotPrefix +
 		"help` to list what commands are available to you. I hope you brought sweets.")
 	if err != nil {
 		_, err = s.ChannelMessageSend(config.BotLogID, err.Error() + "\n" + misc.ErrorLocation(err))
