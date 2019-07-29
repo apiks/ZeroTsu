6f00ae9592a7102156563c108c542e1bb54b1345
apiks <apiks@users.noreply.github.com>
2019-07-29 16:16:24 +0300

    Removed message from about

--- commands/about.go
+++ commands/about.go
@@ -11,7 +11,7 @@ import (
 
 // Returns a message on "about" for BOT information
 func aboutCommand(s *discordgo.Session, m *discordgo.Message) {
-	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hello, I'm %v and was made by Professor Apiks. He says I'm from Darling in the Franxx." +
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hello, I'm %v and was made by Professor Apiks." +
 		" I'm written in Go. Use `%vhelp` to list what commands are available to you.", s.State.User.Username, config.BotPrefix))
 	if err != nil {
 		_, err = s.ChannelMessageSend(config.BotLogID, err.Error() + "\n" + misc.ErrorLocation(err))
