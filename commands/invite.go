5445890270b0acdf74e192d59e567a13ad5982a5
apiks <apiks@users.noreply.github.com>
2019-08-23 20:38:46 +0300

    Added invite command

---
+++ commands/invite.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+
+	"github.com/r-anime/ZeroTsu/misc"
+)
+
+// Prints Public ZeroTsu's invite link
+func inviteCommand(s *discordgo.Session, m *discordgo.Message) {
+
+	inviteLink := "https://discordapp.com/oauth2/authorize?client_id=614495694769618944&permissions=334851414&scope=bot"
+
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Invite me to your server by using this link!\n\n<%v>", inviteLink))
+	if err != nil {
+		misc.MapMutex.Lock()
+		guildBotLog := misc.GuildMap[m.GuildID].GuildConfig.BotLog.ID
+		misc.MapMutex.Unlock()
+		misc.CommandErrorHandler(s, m, err, guildBotLog)
+		return
+	}
+
+}
+
+func init() {
+	add(&command{
+		execute:  inviteCommand,
+		trigger:  "invite",
+		aliases:  []string{"inv", "invit"},
+		desc:     "Print the BOT's invite link",
+		category: "normal",
+	})
+}
