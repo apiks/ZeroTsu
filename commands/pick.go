cd325dd8049672f15a153ca5f835eb6529c00096
apiks <apiks@users.noreply.github.com>
2019-08-30 13:43:17 +0300

    Added pick command

---
+++ commands/pick.go
@@ -0,0 +1,52 @@
+package commands
+
+import "strings"
+
+// Picks one item from a specified number of item.
+func pickCommand(s *discordgo.Session, m *discordgo.Message) {
+
+	misc.MapMutex.Lock()
+	guildBotLog := misc.GuildMap[m.GuildID].GuildConfig.BotLog.ID
+	guildPrefix := misc.GuildMap[m.GuildID].GuildConfig.Prefix
+	misc.MapMutex.Unlock()
+
+	commandStrings := strings.SplitN(m.Content, " ", 2)
+
+	if len(commandStrings) == 1 {
+		_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Usage: `%vpick [item], [item]...`\n\nItem is anything that does not contain `,`\nUse `|` insead of `,` if you need a comma in the item", guildPrefix))
+		if err != nil {
+			misc.CommandErrorHandler(s, m, err, guildBotLog)
+			return
+		}
+		return
+	}
+
+	// Splits each item individually
+	items := strings.Split(commandStrings[1], "|")
+	if len(items) == 1 {
+		items = strings.Split(commandStrings[1], ",")
+	}
+
+	// Trims trailing and leading whitespace from each item
+	for i := 0; i < len(items); i++ {
+		items[i] = strings.TrimSpace(items[i])
+	}
+
+	// Picks a random item
+	randomItemNum := rand.Intn(len(items))
+	_, err := s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Picked: `%v`", items[randomItemNum]))
+	if err != nil {
+		misc.CommandErrorHandler(s, m, err, guildBotLog)
+		return
+	}
+}
+
+func init() {
+	add(&command{
+		execute:  pickCommand,
+		trigger:  "pick",
+		aliases:  []string{"pic", "pik", "p"},
+		desc:     "Picks one thing from a specified number of things.",
+		category: "normal",
+	})
+}
