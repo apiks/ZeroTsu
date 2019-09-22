412f622f3c71727dbca343012f44abf496fa50e4
apiks <apiks@users.noreply.github.com>
2019-09-22 19:51:57 +0300

    Prettified ping command

--- commands/ping.go
+++ commands/ping.go
@@ -1,25 +1,63 @@
 package commands
 
 import (
+	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"log"
+	"time"
 
 	"github.com/r-anime/ZeroTsu/misc"
 )
 
-// Returns a message on "ping" to see if bot is alive
+// Prints a message to see if the BOT is alive
 func pingCommand(s *discordgo.Session, m *discordgo.Message) {
 	misc.MapMutex.Lock()
-	_, err := s.ChannelMessageSend(m.ChannelID, misc.GuildMap[m.GuildID].GuildConfig.PingMessage)
+	err := pingEmbed(s, m)
 	if err != nil {
-		_, err = s.ChannelMessageSend(misc.GuildMap[m.GuildID].GuildConfig.BotLog.ID, err.Error()+"\n"+misc.ErrorLocation(err))
-		if err != nil {
-			misc.MapMutex.Unlock()
-			return
+		if m.GuildID != "" {
+			guildBotLog := misc.GuildMap[m.GuildID].GuildConfig.BotLog.ID
+			misc.CommandErrorHandler(s, m, err, guildBotLog)
 		}
-		misc.MapMutex.Unlock()
-		return
 	}
 	misc.MapMutex.Unlock()
+}
+
+func pingEmbed(s *discordgo.Session, m *discordgo.Message) error {
+	embed := &discordgo.MessageEmbed{
+		Title:       ":ping_pong:",
+		Description: fmt.Sprintf("\n%v", misc.GuildMap[m.GuildID].GuildConfig.PingMessage),
+		Color:       16758465,
+		Thumbnail: &discordgo.MessageEmbedThumbnail {
+			URL:s.State.User.AvatarURL("256"),
+		},
+	}
+
+	// Parses and edits message with how long it took to send message
+	now := time.Now()
+	embedMsg, err := s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	if err != nil {
+		return err
+	}
+	delay, err := embedMsg.Timestamp.Parse()
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	difference := delay.Sub(now).Truncate(time.Millisecond).String()
+	embed = &discordgo.MessageEmbed{
+		Title:       fmt.Sprintf(":ping_pong: %v", difference),
+		Description: fmt.Sprintf("%v", misc.GuildMap[m.GuildID].GuildConfig.PingMessage),
+		Color:       16758465,
+		Thumbnail: &discordgo.MessageEmbedThumbnail {
+			URL:s.State.User.AvatarURL("256"),
+		},
+	}
+	_, err = s.ChannelMessageEditEmbed(embedMsg.ChannelID, embedMsg.ID, embed)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func init() {
@@ -27,7 +65,7 @@ func init() {
 		execute:  pingCommand,
 		trigger:  "ping",
 		aliases:  []string{"pingme"},
-		desc:     "Am I alive?",
+		desc:     "See if I respond and how fast",
 		elevated: true,
 		category: "misc",
 	})
