16b580e5a659b82954fed8467807d58ab3da6eaf
Apiks <alexander.lr.vichev@gmail.com>
2023-02-14 17:32:31 +0200

    Update botlists.go

--- functionality/botlists.go
+++ functionality/botlists.go
@@ -25,23 +25,20 @@ func SendServers(guildCountStr string, s *discordgo.Session) {
 
 // Sends guild count to top.gg
 func discordBotsGuildCount(client *http.Client, guildCount, shardCount string) {
-
 	data := url.Values{
 		"server_count": {guildCount},
 		"shard_count":  {shardCount},
 	}
 	req, err := http.NewRequest("POST", "https://top.gg/api/bots/614495694769618944/stats", bytes.NewBufferString(data.Encode()))
 	if err != nil {
-		log.Println("top.gg Err")
-		log.Println(err)
+		log.Println("top.gg Err:", err)
 		return
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 	req.Header.Add("Authorization", config.DiscordBotsSecret)
 	response, err := client.Do(req)
 	if err != nil {
-		log.Println("top.gg Err")
-		log.Println(err)
+		log.Println("top.gg Err:", err)
 		return
 	}
 	response.Body.Close()
